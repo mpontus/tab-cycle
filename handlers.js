269d2e6855e45d169325387c934835870ced7e22
Michael Pontus <m.pontus@gmail.com>
2017-08-20 16:26:45 +0300

    Split the main script into two files: handlers and commands.

--- handlers.js
+++ handlers.js
@@ -31,35 +31,6 @@ const handleWindowFocus = async (windowId) => {
   }
 }
 
-const buryActiveTab = async () => {
-  const [activeTab] = await browser.tabs.query({ currentWindow: true, active: true });
-
-  browser.tabs.move(activeTab.id, { index: -1 });
-};
-
-const raiseLastTab = async () => {
-  const tabs = await browser.tabs.query({ currentWindow: true });
-  const lastTab = tabs[tabs.length - 1];
-
-  if (lastTab.active !== true) {
-    browser.tabs.update(lastTab.id, { active: true });
-  }
-}
-
-browser.commands.onCommand.addListener((command) => {
-  switch (command) {
-    case 'bury-active-tab':
-      buryActiveTab();
-
-      return;
-
-    case 'raise-last-tab':
-      raiseLastTab();
-
-      return;
-  }
-});
-
 browser.tabs.onActivated.addListener(handleTabActivated);
 browser.tabs.onMoved.addListener(handleTabMoved);
 browser.windows.onFocusChanged.addListener(handleWindowFocus);
