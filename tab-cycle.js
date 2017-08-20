2a1a6e8eb8a91af81e20c5a2c72b0518519767c2
Michael Pontus <m.pontus@gmail.com>
2017-08-20 16:09:23 +0300

    Semantics update

--- tab-cycle.js
+++ tab-cycle.js
@@ -6,24 +6,27 @@ const handleTabActivated = ({ tabId }) => {
 }
 
 const handleTabMoved = async () => {
-  const [leadingTab] = await browser.tabs.query({ currentWindow: true, index: 0 });
+  const [nowLeadingTab] = await browser.tabs.query({ currentWindow: true, index: 0 });
+  const { id, active } = nowLeadingTab;
 
-  browser.tabs.update(leadingTab.id, { active: true });
+  if (!active) {
+    browser.tabs.update(id, { active: true });
+  }
 };
 
 const handleActiveTabOnInitialize = (tab) => {
   const { id, index } = tab;
 
-  if (index !== 0) {
+  if (index > 0) {
     bringTabForward(id);
   }
 };
 
-const handleWindowActivated = async (windowId) => {
+const handleWindowFocus = async (windowId) => {
   const [activeTab] = await browser.tabs.query({ windowId, active: true });
   const { id, index } = activeTab;
 
-  if (index !== 0) {
+  if (index > 0) {
     bringTabForward(id);
   }
 }
@@ -35,10 +38,12 @@ const buryActiveTab = async () => {
 };
 
 const raiseLastTab = async () => {
-  const [lastTab] = await browser.tabs.query({ currentWindow: true, active: true });
+  const tabs = await browser.tabs.query({ currentWindow: true });
+  const lastTab = tabs[tabs.length - 1];
 
-  browser.tabs.update(lastTab.id, { active: true });
-
+  if (lastTab.active !== true) {
+    browser.tabs.update(lastTab.id, { active: true });
+  }
 }
 
 browser.commands.onCommand.addListener((command) => {
@@ -46,18 +51,18 @@ browser.commands.onCommand.addListener((command) => {
     case 'bury-active-tab':
       buryActiveTab();
 
-      break;
+      return;
 
     case 'raise-last-tab':
       raiseLastTab();
 
-      break;
+      return;
   }
 });
 
 browser.tabs.onActivated.addListener(handleTabActivated);
 browser.tabs.onMoved.addListener(handleTabMoved);
-browser.windows.onFocusChanged.addListener(handleWindowActivated);
+browser.windows.onFocusChanged.addListener(handleWindowFocus);
 browser.tabs.query({ currentWindow: true, active: true })
   .then(tabs => tabs[0])
   .then(handleActiveTabOnInitialize);
